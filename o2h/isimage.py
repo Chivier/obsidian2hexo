40dc998db40282185bae8741be01ce1d86d076a8
Chivier Humber <chivier.humber@outlook.com>
2022-01-22 02:20:39 +0800

    version 0.1.0 with python packed

--- o2h/isimage.py
+++
@@ -1,5 +0,0 @@
-
-
-
-def isImage(fileName):
-
--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 import pathlib
 from setuptools import setup, find_packages
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
 
-# The text of the README file
-README = (HERE / "README.md").read_text()
+setup(
+    name="o2h",
+    version="0.1.0",
+    author="Chivier Humber",
+    entry_points={
+        "console_scripts": ["obs2hexo=o2h.o2h:o2h"],
+    },
+    license="MIT",
+    keywords="translator",
+    packages=find_packages(),
+)
