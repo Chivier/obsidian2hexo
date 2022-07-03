242a1583ea7213063c78a64c51a8be611553dd00
Chivier Humber <chivier.humber@outlook.com>
2022-07-03 13:29:43 +0800

    Update tags & Category System

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="o2h",
-    version="0.1.0",
+    version="1.0.0",
     author="Chivier Humber",
     entry_points={
         "console_scripts": ["obs2hexo=o2h.o2h:o2h"],
